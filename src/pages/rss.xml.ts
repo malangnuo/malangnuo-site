033ffdbe3500f1d5e72f8f2a6b2067e61d0cded7
malangnuo <malangnuo@gmail.com>
2026-02-26 14:17:22 +0900

    update title: 말랑 누오 -> 말랑누오

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context: any) {
     return rss({
-        title: "말랑 누오 | 말랑 누오의 기록",
+        title: "말랑누오 | 말랑누오의 기록",
         description: '글이 자라나는 곳 🌱',
         site: context.site,
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
