aac220c9d5f02d7d2ca9e24a0a802e455b3f2a35
malangnuo <malangnuo@gmail.com>
2026-02-26 15:13:04 +0900

    Update var

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import rehypeMathjax from 'rehype-mathjax';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
     markdown: {
