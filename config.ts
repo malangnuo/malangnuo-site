dfc67f915b84b6e2c12fb0e3699a05c4f7bb2737
malangnuo <malangnuo@gmail.com>
2026-02-26 14:50:15 +0900

    Update site_description

--- config.ts
+++ config.ts
@@ -9,13 +9,12 @@ interface Config {
     email: string;
     email_subject: string;
     emailLink: string;
-    friendlink: { [key: string]: string };
 }
 
 const config: Config = {
-    site_title: '말랑 누오의 기록',
-    site_description: '글이 자라나는 곳 🌱',
-    site_tagline: '글이 자라나는 곳 🌱',
+    site_title: '말랑누오의 기록',
+    site_description: '',
+    site_tagline: '',
     avatar: '/avatar.webp',
     author: '말랑누오',
     author_description: '누오처럼 살고 싶어 \n말랑하게 정리해보는 이야기들',
@@ -24,9 +23,6 @@ const config: Config = {
     email_subject: 'To.말랑누오',
     get emailLink(): string {
         return `mailto:${this.email}?subject=${this.email_subject}`;
-    },
-    friendlink: {
-        // "BlockLune's Blog": 'https://blocklune.cc',
     },
 };
 
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context: any) {
     return rss({
         title: "말랑누오 | 말랑누오의 기록",
-        description: '글이 자라나는 곳 🌱',
+        description: '',
         site: context.site,
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
         customData: `<language>en-us</language>`,
