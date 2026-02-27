7c695230ab0d9bff3906c9849a6d4fb847fad0b0
malangnuo <malangnuo@gmail.com>
2026-02-27 10:21:34 +0900

    Update mainpage

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 interface Config {
     site_title: string;
-    site_description: string;
     site_tagline: string;
+    site_tagline2: string;
     avatar: string;
     author: string;
     author_description: string;
@@ -13,11 +13,11 @@ interface Config {
 
 const config: Config = {
     site_title: '말랑누오의 기록',
-    site_description: '',
-    site_tagline: '',
+    site_tagline: '누오처럼 살고 싶어',
+    site_tagline2: '말랑하게 정리해보는 이야기들',
     avatar: '/avatar.webp',
     author: '말랑누오',
-    author_description: '누오처럼 살고 싶어 \n말랑하게 정리해보는 이야기들',
+    author_description: '언제나 여유롭고 말랑한 누오를 좋아합니다.',
     github: 'https://github.com/malangnuo',
     email: 'choiboyuna@gmail.com',
     email_subject: 'To.말랑누오',
